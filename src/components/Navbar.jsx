5d69e05dd3b6a994464f2825538bec2fca415528
Mutegetsi prince <mutegetsi2000@gmail.com>
2022-01-15 18:26:16 +0200

    finished the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
 import logo from '../../images/logo.png'
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+
+const NavBarItem = ({title,classProps}) => {
+    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+}
+
 const Navbar = () => {
     return (
-        <div>
-            The navbar
-        </div>
+        <nav className="w-full flex md:justify-between items-center p-4">
+             <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+
+
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                    {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                            <NavBarItem key={item + index} title={item} />
+                ))}
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                Login
+                </li>
+            </ul>
+
+
+        </nav>
     )
 }
 
