b0732d410b38b59f5c162b87f4b44bf19706f477
Mutegetsi prince <mutegetsi2000@gmail.com>
2022-01-27 20:30:01 +0200

    feat: Added table of transactions

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,69 +3,148 @@ import React from 'react';
 const Table = () => {
   return (
     
-    <div class="flex flex-col">
+    <div class="flex justify-flex items-center flex-col">
       <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
+          <div class="shadow overflow-hidden border-gray-900 ">
+            <table class="min-w-full divide-x border border-gray-900 divide-gray-900">
               <thead class="bg-black">
                 <tr>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Address From
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Address To
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Amount
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Charge Fees
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Token
                   </th>
-                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  <th scope="col" class="px-3 py-1 border-gray-900 border text-left text-xs  text-white uppercase tracking-wider">
                     Timestamp
                   </th>
-                  <th scope="col" class="relative px-6 py-3">
+                  {/* <th scope="col" class="relative px-6 py-3">
                     <span class="sr-only">Edit</span>
-                  </th>
+                  </th> */}
                 </tr>
               </thead>
               <tbody class="bg-black divide-y divide-gray-200">
                 <tr>
-                  <td class="px-6 py-4 whitespace-nowrap">
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
                     <div class="flex items-center">
-                      <div class="flex-shrink-0 h-10 w-10">
+                      {/* <div class="flex-shrink-0 h-10 w-10">
                         <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""/>
-                      </div>
+                      </div> */}
                       <div class="ml-4">
                         <div class="text-sm font-medium text-white">
-                          Jane Cooper
+                          Lorem ipsum dolor sit amet consectetur adipisicing
                         </div>
-                        <div class="text-sm text-white">
+                        {/* <div class="text-sm text-white">
                           jane.cooper@example.com
-                        </div>
+                        </div> */}
                       </div>
                     </div>
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="text-sm text-white">Regional Paradigm Technician</div>
-                    <div class="text-sm text-white">Optimization</div>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    <div class="text-sm text-white">Lorem ipsum dolor sit amet consectetur adipisicing</div>
+                    {/* <div class="text-sm text-white">Optimization</div> */}
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    {/* <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                       Active
-                    </span>
+                    </span> */}
+                    <span class="text-white text-xs">20 ISH</span>
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
                     Admin
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
                     Admin
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-white">
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  {/* <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                  </td> */}
+                </tr>
+                <tr>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    <div class="flex items-center">
+                      {/* <div class="flex-shrink-0 h-10 w-10">
+                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""/>
+                      </div> */}
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-white">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                        </div>
+                        {/* <div class="text-sm text-white">
+                          jane.cooper@example.com
+                        </div> */}
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    <div class="text-sm text-white">Lorem ipsum dolor sit amet consectetur adipisicing</div>
+                    {/* <div class="text-sm text-white">Optimization</div> */}
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    {/* <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      Active
+                    </span> */}
+                    <span class="text-white text-xs">20 ISH</span>
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  {/* <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                  </td> */}
+                </tr>
+                <tr>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    <div class="flex items-center">
+                      {/* <div class="flex-shrink-0 h-10 w-10">
+                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""/>
+                      </div> */}
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-white">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                        </div>
+                        {/* <div class="text-sm text-white">
+                          jane.cooper@example.com
+                        </div> */}
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    <div class="text-sm text-white">Lorem ipsum dolor sit amet consectetur adipisicing</div>
+                    {/* <div class="text-sm text-white">Optimization</div> */}
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap">
+                    {/* <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      Active
+                    </span> */}
+                    <span class="text-white text-xs">20 ISH</span>
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
+                    Admin
+                  </td>
+                  <td class="px-6 py-2 border border-gray-900 whitespace-nowrap text-sm text-white">
                     Admin
                   </td>
                   {/* <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -27,8 +27,8 @@ const Welcome = () => {
         <div className="flex w-full flex-col md:flex-row justify-center items-center main">
              <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
              <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
-                <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                    Send Crypto <br /> across the Rwanda
+                <h1 className="text-3xl sm:text-4xl text-white text-gradient py-1">
+                    Send Crypto  across the Rwanda
                 </h1>
                 <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                     Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
